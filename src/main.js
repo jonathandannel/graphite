fe272836e145476a4a1335ca0ce99ecedd462701
jonathan <usixjad@gmail.com>
2018-05-14 15:31:54 +0000

    added basic skeleton for table

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
-//var drawBarChart = function(data, options, element) {
-//};
-
 var sampleData = [1, 2, 3, 4, 5];
-var chart = document.getElementById('graphite-chart');
+
+var chart = document.querySelector(".graphite-chart");
+
+var table = document.createElement("table");
+table.className = "graphite-skeleton";
+chart.append(table);
 
 var addColumns = function(data) {
+  var row = document.createElement("tr");
+  table.append(row);
   for (i = 0; i < data.length; i++) {
-    var column = document.createElement("div");
+    var column = document.createElement("td");
     column.className = "individual-column";
     column.Id = 'column-' + (i + 1);
     column.innerHTML = column.Id;
-    chart.append(column);
+    row.append(column);
+
   }
 };
-
 
 addColumns(sampleData);
