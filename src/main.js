987e031ae145e9fef83f9298e4faf1f3b7b6e876
jonathan <usixjad@gmail.com>
2018-05-14 10:44:43 +0000

    updated addColumns function

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@
 var sampleData = [1, 2, 3, 4, 5];
 var chart = document.getElementById('graphite-chart');
 
-var addColumns = function(col) {
-  for (i = 0; i < col; i++) {
+var addColumns = function(data) {
+  for (i = 0; i < data.length; i++) {
     var column = document.createElement("div");
     column.className = "individual-column";
+    column.Id = 'column-' + (i + 1);
+    column.innerHTML = column.Id;
     chart.append(column);
   }
 };
 
-addColumns(sampleData.length);
+
+addColumns(sampleData);
