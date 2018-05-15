e15208ccf4e62e2bf7d12af0df39762fb1858ac0
jonathan <usixjad@gmail.com>
2018-05-15 00:51:03 +0000

    started adding css rules for addColumns function

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-var sampleData = [1, 2, 3, 4, 5];
+var sampleData = [4, 6, 7, 1, 24, 22];
 
-var chart = document.querySelector(".graphite-chart");
+var mainContainer = document.querySelector(".graphite-container");
 
-var table = document.createElement("table");
-table.className = "graphite-skeleton";
-chart.append(table);
+var graphiteChart = document.createElement("table");
+graphiteChart.className = "graphite-chart";
+mainContainer.append(graphiteChart);
 
 var addColumns = function(data) {
   var row = document.createElement("tr");
-  table.append(row);
+  graphiteChart.append(row);
   for (i = 0; i < data.length; i++) {
     var column = document.createElement("td");
-    column.className = "individual-column";
+    column.className = "graphite-column";
     column.Id = 'column-' + (i + 1);
     column.innerHTML = column.Id;
+    $(column).css({
+      "background-color": "#9933ff",
+      "height": "400px"
+    });
     row.append(column);
 
   }
