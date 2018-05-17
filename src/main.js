2c0089ef1f93f13f5ffa121ba565f09b67e22d8f
jonathan <usixjad@gmail.com>
2018-05-17 14:08:14 +0000

    created drawBarChart function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var userDataParams = {
+var userData = {
   'black': 12,
   'latino': 8,
   'native': 4,
@@ -7,58 +7,63 @@ var userDataParams = {
   'indian': 6
 };
 
-var mainContainer = document.querySelector(".graphite-chart");
-$(mainContainer).css("padding-left", "50px");
+var drawBarChart = function(data, options, element) {
+  var mainContainer = document.getElementById("graphite-chart-1");
+  $(mainContainer).css("padding-left", "50px");
 
-var addColumns = function(dataObj) {
-  var specs = Object.values(dataObj);
-  for (i = 0; i < specs.length; i++) {
-    var column = document.createElement("div");
-    column.className = "graphite-column";
-    column.Id = 'column-' + (i + 1);
-    column.innerHTML = '<br>' + specs[i];
+  var addColumns = function(dataObj) {
+    var specs = Object.values(dataObj);
+    for (i = 0; i < specs.length; i++) {
+      var column = document.createElement("div");
+      column.className = "graphite-column";
+      column.Id = 'column-' + (i + 1);
+      column.innerHTML = '<br>' + specs[i];
 
-    var columnValue = '';
-    columnValue += (specs[i] * 20) + 'px';
-    $(column).css({
-      "display": "inline-block",
-      "height": columnValue,
-      "background-color": "#4d88ff",
-      "vertical-align": "bottom",
-      "margin-right": "30px",
-      "width": "90px",
-      "text-align": "center",
-      "font-family": "arial",
-      "text-padding": "5px",
-      "font-size": "14px"
-    });
-    $(mainContainer).append(column);
-  }
+      var columnValue = '';
+      columnValue += (specs[i] * 20) + 'px';
+      $(column).css({
+        "display": "inline-block",
+        "height": columnValue,
+        "background-color": "#4d88ff",
+        "vertical-align": "bottom",
+        "margin-right": "30px",
+        "width": "90px",
+        "text-align": "center",
+        "font-family": "arial",
+        "text-padding": "5px",
+        "font-size": "14px"
+      });
+      $(mainContainer).append(column);
+    }
+  };
+
+  var addChartBottom = function (dataObj) {
+    var specs = Object.keys(dataObj);
+
+    var chartBottom = document.createElement("div");
+    chartBottom.className = "graphite-chart-bottom";
+    $(mainContainer).append(chartBottom);
+
+    for (i = 0; i < specs.length; i++) {
+      var labelName = specs[i];
+      var bottomLabel = document.createElement("div");
+      bottomLabel.className = "graphite-chart-bottom-label";
+      bottomLabel.innerHTML = labelName;
+      $(bottomLabel).css({
+        "display": "inline-block",
+        "padding-top": "10px",
+        "margin-right": "30px",
+        "text-align": "center",
+        "width": "90px",
+        "font-family": "arial"
+      });
+      $(chartBottom).append(bottomLabel);
+    }
+  };
+
+  addColumns(data);
+  addChartBottom(data);
+
 };
 
-var addChartBottom = function (dataObj) {
-  var specs = Object.keys(dataObj);
-
-  var chartBottom = document.createElement("div");
-  chartBottom.className = "graphite-chart-bottom";
-  $(mainContainer).append(chartBottom);
-
-  for (i = 0; i < specs.length; i++) {
-    var labelName = specs[i];
-    var bottomLabel = document.createElement("div");
-    bottomLabel.className = "graphite-chart-bottom-label";
-    bottomLabel.innerHTML = labelName;
-    $(bottomLabel).css({
-      "display": "inline-block",
-      "padding-top": "10px",
-      "margin-right": "30px",
-      "text-align": "center",
-      "width": "90px",
-      "font-family": "arial"
-    });
-    $(chartBottom).append(bottomLabel);
-  }
-};
-
-addColumns(userDataParams);
-addChartBottom(userDataParams);
+drawBarChart(userData, 0, "graphite-chart-1");
