d07152eafd6bda5502f5dc3b0791b72939fe7789
jonathan <usixjad@gmail.com>
2018-05-18 15:43:17 +0000

    separator now inside column function

--- src/main.js
+++ src/main.js
@@ -1,59 +1,41 @@
 var userData = {
-  'black': 12,
+  'black': 15,
   'latino': 8,
   'native': 4,
   'asian': 8,
-  'white': 16,
+  'white': 8,
   'indian': 6,
   'other': 5
 };
 
 var drawBarChart = function(data, options, element) {
+  var highestNumberPx = 0;
   var mainContainer = document.getElementById(element);
   $(mainContainer).css({
-    "margin-left": "10px"
+    "margin-left": "10px",
+    "position": "relative"
   });
-
-  var addSeparators = function(dataObj) {
-
-    var specs = Object.values(dataObj);
-    var highestNumber = 0;
-    for (i = 0; i < specs.length; i++) {
-      if (specs[i] > highestNumber) {
-        highestNumber = specs[i];
-      }
-    }
-
-    var interval = Math.round(highestNumber * 20) / 5;
-    console.log(interval);
-
-    for (i = 1; i < 6; i++) {
-      var separator = document.createElement("hr");
-      $(separator).css({
-        "position": "absolute",
-        "width": "100%",
-        "margin-top": (interval * i) + "px"
-      });
-      $(mainContainer).append(separator);
-    }
-
-
-  };
 
   var addColumns = function(dataObj) {
     var specs = Object.values(dataObj);
+    var highestPixel = 0;
+
     for (i = 0; i < specs.length; i++) {
       var column = document.createElement("div");
       column.className = "graphite-column";
       column.Id = 'column-' + (i + 1);
       column.innerHTML = '<br>' + specs[i];
+      var columnValue = 0;
 
-      var columnValue = '';
-      columnValue += (specs[i] * 20) + 'px';
+      columnValue = (specs[i] * 20);
+      if (columnValue > highestPixel) {
+        highestPixel = columnValue;
+      }
+
       $(column).css({
         "display": "inline-block",
         "position": "relative",
-        "height": columnValue,
+        "height": columnValue + "px",
         "margin-right": "30px",
         "width": "90px",
         "background-color": "#4d88ff",
@@ -66,7 +48,26 @@ var drawBarChart = function(data, options, element) {
       });
       $(mainContainer).append(column);
     }
+
+    var addSeparators = function (dataObj, pixelHeight) {
+      var heightdivided = Math.round(pixelHeight / 4);
+      console.log(pixelHeight);
+
+      for (i = 0; i <= pixelHeight; i += heightdivided) {
+        var separator = document.createElement("hr");
+        console.log(i);
+        $(separator).css({
+          "position": "absolute",
+          "width": "100%",
+          "margin-top": i + "px",
+          "top": "0"
+        });
+        $(mainContainer).append(separator);
+      }
+    };
+    addSeparators(data, highestPixel);
   };
+
 
   var addChartBottom = function (dataObj) {
     var specs = Object.keys(dataObj);
@@ -93,7 +94,7 @@ var drawBarChart = function(data, options, element) {
       $(chartBottom).append(bottomLabel);
     }
   };
-  addSeparators(data);
+  console.log(highestNumberPx);
   addColumns(data);
   addChartBottom(data);
 };
