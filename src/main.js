2840edc6e749b81be3c6a8468d1de52835134a4e
jonathan <usixjad@gmail.com>
2018-05-24 04:49:04 +0000

    testing width scaling boundaries

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 var userData = {
-  'Io': 4,
-  'Europa': 7.45,
-  'Ganymede': 6,
-  'Callisto': 12,
-  'Enceladus': 8.1,
-  'Titan': 4.91,
-  'Dione': 3.750
+  'one': 4,
+  'two': 4.90,
+  'three': 3.29,
+  'four': 2.51,
+  'five': 2.44,
+  'six': 8.3,
+  'seven': 4.02
 };
 
 var userOptions = {
@@ -16,10 +16,44 @@ var userOptions = {
 };
 
 var drawBarChart = function(data, options, element) {
-  console.log(options.barColor);
-  var containerHeight = 400;
-  var containerWidth = 900;
+  var containerHeight = 200;
+  var containerWidth = 450;
   var barColor = '';
+  var title = '';
+
+  //width of 600 can fit max 10 columns
+  //width of 500 can fit max 8 columns
+  //width of 400 can fit max 6 columns
+  //width 350 can fit 6
+  //width of 300 can fit max 5 columns
+  //width of 250 can fit 4
+  //width of 200 can fit max 2 columns
+
+  var handleUserDimensions = function(dataObject, optionsObject) {
+    var keyCount = Object.keys(dataObject);
+    var width = optionsObject.width;
+    var minWidth = 200;
+    if (keyCount.length) {
+      console.log(null);
+
+    }
+  };
+
+  var setDefaultDimensions = function(dataObject) {
+    var l;
+  };
+
+  var setUserOptions = function(_options) {
+    if (_options.width < 400) {
+      containerWidth = 900;
+    } else if (_options.height < 200) {
+      containerHeight = 400;
+    } else if (options.title.length < 1) {
+      title = "Untitled Graph";
+    }
+  };
+
+
   var mainContainer = document.createElement("div");
   $(mainContainer).css({
     "position": "relative",
@@ -34,7 +68,6 @@ var drawBarChart = function(data, options, element) {
     "color": "#4c4a4c"
   });
 
-  var title = "Saturnian/Jovian moon densities (g/cm²) ";
   var graphTitle = document.createElement("div");
   graphTitle.innerHTML = "<h1>" + title + "</h1>";
   $(graphTitle).css({
@@ -66,7 +99,7 @@ var drawBarChart = function(data, options, element) {
       "position": "relative",
       "padding-top": "8px",
       "padding-bottom": "9px",
-      "margin-left": "50px"
+      "margin-left": "40px"
     });
 
     for (i = 0; i < values.length; i++) {
@@ -78,7 +111,8 @@ var drawBarChart = function(data, options, element) {
         "background": "linear-gradient(rgb(189, 168, 193), rgb(206, 192, 209))",
         "text-align": "center",
         "height": values[i] * (height / maxHeight) - 1 + "px",
-        "z-index": "2"
+        "z-index": "2",
+        "color": "#383434"
       });
       var columnLabel = document.createElement("div");
       columnLabel.innerHTML = Object.keys(dataObject)[i];
