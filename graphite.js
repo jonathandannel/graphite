e06865932d0012e193de3bc3ff4de8a65bdd05a6
jonathan <usixjad@gmail.com>
2018-05-25 20:25:06 +0000

    comments added

--- graphite.js
+++ graphite.js
@@ -1,5 +1,6 @@
 function graphite(data, options, element) {
 
+  /* Make sure user's width parameter meets minimum requirements to support the amount of bars. */
   var handleUserWidth = function(_data, _options) {
     var keyCount = Object.keys(_data).length;
     var minWidth;
@@ -17,6 +18,7 @@ function graphite(data, options, element) {
     }
   };
 
+  /* Calculate what the default width should be if user does not enter a width */
   var getDefaultWidth = function(_data) {
     var keyCount = Object.keys(_data).length;
     x = keyCount - 3;
@@ -24,10 +26,12 @@ function graphite(data, options, element) {
     return minWidth;
   };
 
+  /* Initialize the dimensions and title of the chart depending on user parameters */
   var containerHeight = options.height ? options.height : 300;
   var containerWidth = options.width ? handleUserWidth(data, options) : getDefaultWidth(data);
   var title = options.title ? options.title : '';
 
+  /* Create a main container to hold graph */
   var mainContainer = document.createElement("div");
   mainContainer.className = "graphite-container";
   $(mainContainer).css({
@@ -55,11 +59,15 @@ function graphite(data, options, element) {
     "font-size": "12px"
   });
 
+  /* Add container to user-selected HTML element */
   $(element).append(graphTitle);
   $(element).append(mainContainer);
 
+  /* Populate columns and x-axis labels */
   var addColumns = function(_data, height, width) {
     var values = Object.values(_data);
+
+    /* Find the highest value for use in scaling the height */
     var maxHeight = 0;
     for (i = 0; i < values.length; i++) {
       if (values[i] > maxHeight) {
@@ -79,6 +87,7 @@ function graphite(data, options, element) {
       "margin-left": "40px"
     });
 
+    /* Width and length of each column is dependent on dimensions of container */
     for (i = 0; i < values.length; i++) {
       var column = document.createElement("div");
       column.innerHTML = '<p>' + values[i];
@@ -105,6 +114,7 @@ function graphite(data, options, element) {
     }
   };
 
+  /* Populate y-axis markers based on container height */
   var addSeparators = function(_data, height) {
     var values = Object.values(_data);
     var maxHeight = 0;
